971f37a45493f96f5dbfc073325a910ce46cbb5f
unknown <bheidebrecht@uni-osnabrueck.de>
2023-05-16 01:28:49 +0200

    add changes to combination file

--- combination.py
+++ combination.py
@@ -1,17 +1,16 @@
-import numpy as np 
+import numpy as np
 
 def combination(a, b, axis=0):
     # Remove unnecessary dimensions by squeezing
     array_a = a.squeeze()
     array_b = b.squeeze()
     
-    # check if the arrays can be combined along the given axis
-    if array_a.shape[axis] != array_b.shape[axis]:
-        return f"Cannot combine arrays along the specified axis. The shape doesn't match."
-        
-    # now combine the two arrays and return the result
-    combined_arrays = np.concatenate((array_a, array_b), axis=axis)
-    return combined_arrays
+    
+    try:
+        combined_arrays = np.concatenate((array_a, array_b), axis)
+        return combined_arrays
+    except ValueError:
+        print("Cannot combine arrays along the specified axis. The shape doesn't match.")
 
 
 if __name__ == "__main__":
